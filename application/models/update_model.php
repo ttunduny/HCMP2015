177389e221444d514d6654c7a19f46c3ba8d9afe
Richard Karsan <karsanrichard@gmail.com>
2016-05-05 20:08:47 +0300

    no message

--- application/models/update_model.php
+++ application/models/update_model.php
@@ -8,7 +8,7 @@ class Update_model extends Doctrine_Record {
 	{
 		$query = Doctrine_Manager::getInstance()->getCurrentConnection()->fetchAll("SELECT * FROM update_log WHERE added_on=(SELECT MAX(added_on) FROM update_log)");
 
-		$return_type = (isset($time_only) && $time_only ==1)? "time_only";
+		$return_type = (isset($time_only) && $time_only ==1)? "time_only":NULL;
 		if ($return_type == 1) {
 			return $query[0]['added_on'];
 		}
