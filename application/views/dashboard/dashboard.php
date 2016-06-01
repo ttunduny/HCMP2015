c08791a76a7363164835d712262f5f41e59a87bc
Richard Karsan <karsanrichard@gmail.com>
2016-06-01 22:34:57 +0300

    Naskia kichwa

--- application/views/dashboard/dashboard.php
+++ application/views/dashboard/dashboard.php
@@ -135,13 +135,13 @@
               <span class="count_top"><i class="fa fa-building-o"></i> Total Facilities | HCMP Online</span>
               <div class="count"><?php echo $facility_count[0]['online_facilities']; ?></div>
               <!-- <span class="count_bottom"><i class="green"><i class="fa fa-sort-desc"></i>12% </i> Nation-wide Coverage</span> -->
-              <span class="count_bottom"> Facilities <b class="green"> Online</b></span>
+              <span class="count_bottom"> Facilities using <b class="green">HCMP Online</b></span>
             </div>
             <div class="col-md-3 col-sm-4 col-xs-6 tile_stats_count">
               <span class="count_top"><i class="fa fa-building"></i> Total Facilities | HCMP Offline</span>
               <div class="count"><?php echo $facility_count[0]['offline_facilities']; ?></div>
               <!-- <span class="count_bottom"><i class="green"><i class="fa fa-sort-asc"></i>23% </i> Percentage Online</span> -->
-              <span class="count_bottom"> Facilities <b class="red"> Offline</b></span>
+              <span class="count_bottom"> Facilities using<b class="red">HCMP Offline</b></span>
             </div>
             
           </div>
