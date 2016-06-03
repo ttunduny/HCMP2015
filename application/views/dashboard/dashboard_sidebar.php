030c497bc4ed9ece2f30b10c86932e2f9f209574
Richard Karsan <karsanrichard@gmail.com>
2016-06-03 10:38:04 +0300

    Dashboard: Sidebar changes

--- application/views/dashboard/dashboard_sidebar.php
+++ application/views/dashboard/dashboard_sidebar.php
@@ -25,7 +25,7 @@
                     <?php endforeach; ?>
                     </ul>
                   </li>
-
+                  <!-- 
                   <li><a><i class="fa fa-bar-chart"></i> Subcounty <span class="fa fa-chevron-down"></span></a>
                     <ul class="nav child_menu">
                       <li><a href="index.html">Subcounty #1</a>
@@ -47,13 +47,13 @@
                       </li>
                     </ul>
                   </li>
-
+                   -->
                 </ul>
               </div>
               <div class="menu_section">
                 <h3>System Access</h3>
                 <ul class="nav side-menu">
-                  <li><a><i class="fa fa-user"></i> Login </a></li>
+                  <li><a href="<?php echo base_url().'user'; ?>"><i class="fa fa-user"></i> Login </a></li>
                 </ul>
               </div>
 
