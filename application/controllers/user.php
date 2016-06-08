0be1042f17cea1d3ae67266ffd862b0e3b8b9143
Richard Karsan <karsanrichard@gmail.com>
2016-06-08 13:02:32 +0300

    Error reporting and test once more

--- application/controllers/user.php
+++ application/controllers/user.php
@@ -136,8 +136,9 @@ class User extends MY_Controller {
 			$u1->facility_code = ($facility_code==0) ? null : $facility_code;
 			$u1->save();
 			
-			echo "I work";exit;
+			error_reporting(1);
 			redirect('home');
+			echo "I work";exit;
 		} else {
 			$data['popup'] = "errorpopup";
 			$data['title'] = "Login";
